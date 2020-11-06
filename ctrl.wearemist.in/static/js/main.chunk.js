85cb32c044079c84d8fa7d6bfa7749230f1c0822
kunal bhatia <kunal18042002@gmail.com>
2020-11-06 07:50:35 +0530

    Update main.chunk.js

--- ctrl.wearemist.in/static/js/main.chunk.js
+++ ctrl.wearemist.in/static/js/main.chunk.js
@@ -329,7 +329,7 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony default export */ __webpack_exports__["default"] = ("Scapetrial/ctrl.wearemist.in/static/mockup.02121f4f.mp4");
+/* harmony default export */ __webpack_exports__["default"] = ("static/mockup.02121f4f.mp4");
 
 /***/ }),
 
